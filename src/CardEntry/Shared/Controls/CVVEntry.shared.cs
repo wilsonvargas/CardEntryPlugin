e231996dc574f92911f338cce10b4e7e1a2b5b45
Wilson Vargas <wilsonvargas_6@outlook.com>
2019-07-04 11:16:51 -0500

    Added format to new controls

--- src/CardEntry/Shared/Controls/CVVEntry.shared.cs
+++ src/CardEntry/Shared/Controls/CVVEntry.shared.cs
@@ -1,11 +1,19 @@
-﻿using System;
+﻿using Forms.Plugin.CardForm.Shared.Behaviors;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using Xamarin.Forms;
 
-namespace Forms.Plugin.CardForm.Shared.Controls
+namespace Forms.Plugin.CardForm.Controls
 {
-    protected class CVVEntry : Entry
+    public class CVVEntry : Entry
     {
+        public CVVEntry()
+        {
+            HeightRequest = 50;
+            Behaviors.Add(new CardBehavior() { Mask = "###" });
+            Keyboard = Keyboard.Numeric;
+            HorizontalOptions = LayoutOptions.FillAndExpand;
+        }
     }
 }
--- src/CardEntry/Shared/Controls/DateEntry.shared.cs
+++ src/CardEntry/Shared/Controls/DateEntry.shared.cs
@@ -1,11 +1,19 @@
-﻿using System;
+﻿using Forms.Plugin.CardForm.Shared.Behaviors;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using Xamarin.Forms;
 
-namespace Forms.Plugin.CardForm.Shared.Controls
+namespace Forms.Plugin.CardForm.Controls
 {
-    protected class DateEntry : Entry
+    public class DateEntry : Entry
     {
+        public DateEntry()
+        {
+            HeightRequest = 50;
+            Behaviors.Add(new CardBehavior() { Mask = "##/##" });
+            Keyboard = Keyboard.Numeric;
+            HorizontalOptions = LayoutOptions.FillAndExpand;
+        }
     }
 }
