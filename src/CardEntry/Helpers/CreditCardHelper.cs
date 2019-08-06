1ac1114e22fe4df1e99140ed0bcff7a680e98aeb
Wilson Vargas <wilsonvargas_6@outlook.com>
2019-08-05 19:43:17 -0500

    cleanup code

--- src/CardEntry/Helpers/CreditCardHelper.cs
+++ src/CardEntry/Helpers/CreditCardHelper.cs
@@ -6,6 +6,10 @@ namespace Forms.Plugin.CardForm.Shared.Helpers
     public class CreditCardHelper
     {
         private const string cardRegex = "^(?:(?<Visa>4[0-9]{1,12}(?:[0-9]{3})?)|(?<mastercard>5[1-5] [0-9]{14})|(?<discover>6(?:011|5[0-9]{2})[0-9]{12})|(?<amex>3[47] [0-9]{13})|(?<diners>3(?:0[0-5]|[68] [0-9])[0-9]{11})|(?<jcb>(?:2131|1800|35[0-9]{3})[0-9]{11}))$";
+        private const string amexRegex = @" ^ 3[47][0 - 9]{1,13}$";
+        private const string masterCardRegex = @"^(?:5[1-5][0-9]{1,2}|222[1-9]|22[3-9][0-9]|2[3-6][0-9]{1,2}|27[01][0-9]|2720)[0-9]{1,12}$";
+        private const string visaRegex = @"^4[0-9]{1,12}(?:[0-9]{1,3})?$";
+        private const string discoverRegex = @"^6(?:011|5[0-9]{2})[0-9]{1,12}$";
 
         public static bool IsValidNumber(string cardNum)
         {
@@ -41,19 +45,19 @@ namespace Forms.Plugin.CardForm.Shared.Helpers
             GroupCollection gc = cardTest.Match(cardNum).Groups;
             cardNum = cardNum.Replace(" ", "").Replace("-", "");
 
-            if (Regex.Match(cardNum, @"^3[47][0-9]{1,13}$").Success)
+            if (Regex.Match(cardNum, amexRegex).Success)
             {
                 return CreditCardTypeType.Amex;
             }
-            else if (Regex.Match(cardNum, @"^(?:5[1-5][0-9]{1,2}|222[1-9]|22[3-9][0-9]|2[3-6][0-9]{1,2}|27[01][0-9]|2720)[0-9]{1,12}$").Success)
+            else if (Regex.Match(cardNum, masterCardRegex).Success)
             {
                 return CreditCardTypeType.MasterCard;
             }
-            else if (Regex.Match(cardNum, @"^4[0-9]{1,12}(?:[0-9]{1,3})?$").Success)
+            else if (Regex.Match(cardNum, visaRegex).Success)
             {
                 return CreditCardTypeType.Visa;
             }
-            else if (Regex.Match(cardNum, @"^6(?:011|5[0-9]{2})[0-9]{1,12}$").Success)
+            else if (Regex.Match(cardNum, discoverRegex).Success)
             {
                 return CreditCardTypeType.Discover;
             }
